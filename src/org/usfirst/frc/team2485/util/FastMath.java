65435c4285823c50b7274d8b2f923c41ba3a4139
Jeremy McCulloch <jeremymcculloch@dell-laptop.attlocal.net>
2018-03-12 23:36:57 -0700

    fixed atan, added sqrt

--- src/org/usfirst/frc/team2485/util/FastMath.java
+++ src/org/usfirst/frc/team2485/util/FastMath.java
@@ -1,16 +1,23 @@
 package org.usfirst.frc.team2485.util;
 
 
+
 public class FastMath {
 	public static final int numSamples = 1000;
 	public static double[] cos;
 	public static double[] acos;
+	public static double[] sqrt;
+	public static double[] atan;
 	public static void init() {
-		cos = new double[numSamples];
-		acos = new double[numSamples];
-		for (int i = 0; i < numSamples; i++) {
+		cos = new double[numSamples + 1];
+		acos = new double[numSamples + 1];
+		sqrt = new double[numSamples + 1];
+		atan = new double[numSamples + 1];
+		for (int i = 0; i <= numSamples; i++) {
 			cos[i] = Math.cos(Math.PI * 2 * i / numSamples);
 			acos[i] = Math.acos(2.0 * i / numSamples - 1);
+			sqrt[i] = Math.sqrt(1 + 3.0 * i / numSamples);
+			atan[i] = Math.atan(1.0 * i / numSamples);
 		}
 		
 	}
@@ -37,7 +44,26 @@ public class FastMath {
 		return Math.PI / 2 - acos(a);
 		
 	}
+	
+	public static double atan(double a) {
+		if (a < 0) {
+			return -atan(-a);
+		} else if (a > 1) {
+			return Math.PI / 2 - atan(1 / a);
+		} else {
+			int index = (int) (a * numSamples);
+			double rem = (a * numSamples) % 1;
+			return atan[index] * (1 - rem) + atan[index + 1] * rem;
+		}
+	}
 
+	public static double atan2(double y, double x) {
+		if (x == 0) {
+			return y > 0 ? Math.PI / 2 : 3 * Math.PI / 2;
+		} else {
+			return x > 0 ? (atan(y / x) + 2 * Math.PI) % (2 * Math.PI): Math.PI + atan(y / x);
+		}
+	}
 	public static double acos(double a) {
 		if (a >= 1) {
 			return 0;
@@ -50,8 +76,19 @@ public class FastMath {
 		}
 	}
 	
-	public static double atan(double a) {
-		return asin(a) / acos(a);
+	public static double sqrt(double a) {
+		double ans = 1;
+		while (a >= 4) {
+			ans *= 2;
+			a /= 4;
+		}
+		while (a < 1) {
+			ans /= 2;
+			a *= 4;
+		}
+		int index = (int) ((a - 1) / 3 * numSamples);
+		double rem = ((a - 1) / 3 * numSamples) % 1;
+		return (sqrt[index] * (1 - rem) + sqrt[index + 1] * rem) * ans;
 	}
 	
 	public static double toRadians(double angle) {
